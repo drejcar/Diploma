46a4c25f228b76a1f41de61d36cce3385d37f0e2
drejcar <drejcar@gmail.com>
2020-01-15 13:42:37 +0100

    Basic views added/updated

--- FortiGate/forms.py
+++ FortiGate/forms.py
@@ -6,47 +6,44 @@ import urllib.request
 from django.http import response
 from django.shortcuts import render
 
+Choices = [('html', 'HTML'), ('ssh', 'SSH'), ('Serial', 'Serial')]
+
 
 class addFortigate(forms.Form):
-    connections=['html','ssh','serial']
-    ipaddress = forms.CharField(label='IPv4 address:',initial='192.168.1.99')
-    username = forms.CharField(label='Username',initial='admin')
-    password = forms.CharField(label='password',initial='', widget=forms.PasswordInput, required=False )
-    contype = forms.RadioSelect(choices=connections)
-    address=str(ipaddress)
+    ipaddress = forms.CharField(label='IPv4 address:', initial='192.168.1.99')
+    username = forms.CharField(label='Username', initial='admin')
+    password = forms.CharField(label='password', initial='', widget=forms.PasswordInput, required=False)
+    contype = forms.ChoiceField(label='Connection Type', choices=Choices, widget=forms.RadioSelect, initial='HTML')
 
+    address = str(ipaddress)
 
-    address='https://'+address
+    address = 'https://' + address
     values = {'username': username,
               'password': password}
 
+#   def clean(self):
+#       cleaned_data = super().clean()
+#       super().ipaddress = cleaned_data.get("ipaddress")
+#       super().username = cleaned_data.get("username")
+#       super().password = cleaned_data.get("password")
+#   clean()
 
 
+#   def search(self, ipaddress,values):
+#       data = urllib.parse.urlencode(values)
+#       data = data.encode('ascii')  # data should be bytes
+#       req = urllib.request.Request(ipaddress, data)
+#       with urllib.request.urlopen(req) as response:
+#           the_page = response.read()
 
-
- #   def clean(self):
- #       cleaned_data = super().clean()
- #       super().ipaddress = cleaned_data.get("ipaddress")
- #       super().username = cleaned_data.get("username")
- #       super().password = cleaned_data.get("password")
- #   clean()
-
-
- #   def search(self, ipaddress,values):
- #       data = urllib.parse.urlencode(values)
- #       data = data.encode('ascii')  # data should be bytes
- #       req = urllib.request.Request(ipaddress, data)
- #       with urllib.request.urlopen(req) as response:
- #           the_page = response.read()
-
- #   result = {}
- #   if response.status_code == 200:  # SUCCESS
- #       result = response.json()
- #       result['success'] = True
- #   else:
- #       result['success'] = False
- #      if response.status_code == 404:  # NOT FOUND
- #           result['message'] = 'No entry found for "%s"' % ipaddress
- #       else:
- #           result['message'] = 'The Firewall is not available at the moment. Please try again later, or try another ip.'
- #   return result
+#   result = {}
+#   if response.status_code == 200:  # SUCCESS
+#       result = response.json()
+#       result['success'] = True
+#   else:
+#       result['success'] = False
+#      if response.status_code == 404:  # NOT FOUND
+#           result['message'] = 'No entry found for "%s"' % ipaddress
+#       else:
+#           result['message'] = 'The Firewall is not available at the moment. Please try again later, or try another ip.'
+#   return result
--- FortiGate/views.py
+++ FortiGate/views.py
@@ -2,19 +2,20 @@ import requests
 # from rest_framework import requests.models.Response
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+
 from urllib3.exceptions import InsecureRequestWarning
 from bs4 import BeautifulSoup
 
 from .forms import addFortigate
 
 
-@api_view(['GET','POST'])
+@api_view(['GET'])
 def newFortigate(request):
   #  form = addFortigate()
  #   template_name='templates/addFortigate.html'
     if request.method == 'GET':
         # create a form instance and populate it with data from the request:
-        form = addFortigate(request.POST)
+        form = addFortigate(request.GET)
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
@@ -24,44 +25,33 @@ def newFortigate(request):
             password = form.cleaned_data['password']
             print(form.cleaned_data['ipaddress'])
             ipaddress = form.cleaned_data['ipaddress']
-
+            print(form.cleaned_data['contype'])
+            Connection=form.cleaned_data['contype']
+            ###testiranje nacinov povezovanja
             #populate the payload with login data
             payload={'username':username,'secretkey':password}
 
             requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-            args = {'form': form, 'username':username, 'password': password, 'ipaddress': ipaddress}
-            #url = 'https://' + ipaddress + '/logincheck?username=' + username + '&secretkey=' + password + '>https://' + ipaddress + '/logincheck?username=' + username + '&secretkey=' + password
+           # args = {'form': form, 'username':username, 'password': password, 'ipaddress': ipaddress}
             url = 'https://' + ipaddress + '/'
             print(url+"\n")
             with requests.Session() as s:
-                #Odziv = s.get(url)
-                #signin = BeautifulSoup(Odziv._content, 'html.parser')
-                    #payload['token_code'] = signin.find('input', id='token_code')['value']
+                requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+                res = s.get(url)
+                #signin = BeautifulSoup(res._content, 'html.parser')
+                #payload['token_code'] = signin.find('input', id='token_code')['value']
                 f=requests.get(url, data=payload, verify=False)
                 a=f.text
                 print(a)
-                return a
-
-
-            #Odziv=requests.post(url, verify=False)
-            #Odziv=requests.get(url, data=payload, verify=False)
-            print(f.text)
+                return f
             return f
-            #return render(request, addFortigate, args)
-            # redirect to a new URL:
-            #return HttpResponseRedirect('/configure/')
 
-            try:
-                return urlopen('https://'+form.cleaned_data['ipaddress'])
-            except URLError as e:
-                if hasattr(e, 'reason'):
-                    print('We failed to reach a server.')
-                    print('Reason: ', e.reason)
-                elif hasattr(e, 'code'):
-                    print('The server couldn\'t fulfill the request.')
-                    print('Error code: ', e.code)
+
+
 
         # if a GET (or any other method) we'll create a blank form
     else:
-        form = addFortigate()
+        return 4
+        #return render(request, 'addFortigate.html', {'form': form})
+        #form = addFortigate()
     return render(request, 'addFortigate.html', {'form': form})
